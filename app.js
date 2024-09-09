ed32f45e08d80720cfeec1bac58c952f5b322478
seyla-song <songseyla99@gmail.com>
2024-09-09 13:57:39 +0700

    feat-jwt: testing signing token

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const authRoutes = require("./routes/auth");
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (req, res) => {
-  console.log(process.env.HOST_DB);
-  const product = await UserAccount.findAll();
-  res.status(200).json({
-    status: "success",
-    data: product,
-  });
-});
+// app.get("/", async (req, res) => {
+//   console.log(process.env.HOST_DB);
+//   const product = await UserAccount.findAll();
+//   res.status(200).json({
+//     status: "success",
+//     data: product,
+//   });
+// });
 
 app.post("/create", async (req, res) => {
   const createItem = await ProductCategory.create(req.body);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,8 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const UserAccount = require("../models/UserAccount");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -12,6 +16,7 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_EXPIRES_COOKIE_IN * 24 * 60 * 60 * 1000,
     ),
+    // this will make the cookie can not be modify or anything from browser
     httpOnly: true,
   };
 
@@ -24,3 +29,39 @@ const createSendToken = (user, statusCode, res) => {
     },
   });
 };
+
+exports.signup = catchAsync(async (req, res, next) => {
+  try {
+    const { username, email, password, passwordConfirm, role } = req.body;
+
+    // Check if passwords match
+    if (password !== passwordConfirm) {
+      return next(new AppError("Passwords do not match!", 400));
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    // Create new user
+    const newUser = await UserAccount.create({
+      username, // Ensure this field is included
+      email,
+      password: hashedPassword,
+      role,
+      user_uid: req.body.user_uid,
+    });
+
+    // Send response
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+
+    // createSendToken(newUser, 201, res);
+  } catch (error) {
+    console.error("Error during user creation:", error);
+    return next(new AppError("Error creating user!", 500));
+  }
+});
