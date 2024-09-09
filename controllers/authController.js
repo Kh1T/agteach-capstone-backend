58baa0d07f9585cea5245843eb480c3b34dbd647
Khit Khom <khomkhit460@gmail.com>
2024-09-09 14:16:41 +0700

    feat-signup-api: delete unuse file and change the hash password to process in middleware.

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,7 @@ const UserAccount = require("../models/UserAccount");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
+
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
