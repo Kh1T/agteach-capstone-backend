8e22c6edcaf3aec67091976a070e253fac80289e
Khit Khom <khomkhit460@gmail.com>
2024-09-10 09:14:00 +0700

    feat-sign-up: Delete unuse code and use errorController in the middleware.

--- utils/appError.js
+++ utils/appError.js
@@ -1,12 +1,10 @@
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
-
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
     this.isOperational = true;
-
-    Error.captureStackTrace(this, this.constructor);
+    Error.captureStackTrace(this, this.constructor); // Ensure stack trace is captured
   }
 }
 
