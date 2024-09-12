e552bb64bc045842ba336c25744b5ae57b63a20a
Khit Khom <khomkhit460@gmail.com>
2024-09-12 13:53:17 +0700

    feat-signup-api: add updateMe func

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
 
 const router = express.Router();
@@ -12,6 +13,8 @@ router.patch("/resetPassword", authController.resetPassword);
 
 router.post("/resend-code", authController.resendVerifyCode);
 router.post("/verify-email", authController.verifyEmail);
+
+router.patch("/updateMe", userController.updateMe);
 
 // router.get("/test", authController.protect, (req, res) => {
 //   res.json({
