c8ccd4f8cbac0327971b5738f82952d2ba5869a4
seyla-song <songseyla99@gmail.com>
2024-09-17 10:35:14 +0700

    fix-reset: testing jest

--- models/customerModel.js
+++ models/customerModel.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
-const UserAccount = require('./userModel');
+
 
 const Customer = sequelize.define('customer', {
     customerId: {
