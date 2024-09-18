465e337d3a6ad0f19376849c0e9d2eea284835bd
Khit Khom <khomkhit460@gmail.com>
2024-09-18 14:52:46 +0700

    feat-getme: create new getme and make UserAccount and Customer Associate.

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' });
 
-const sequelize = require("./config/db");
-const app = require("./app");
+const sequelize = require('./config/db');
+const app = require('./app');
 
 try {
   const authenticate = async () => await sequelize.authenticate();
   authenticate();
-  console.log("Connection has been established successfully.");
+  console.log('Connection has been established successfully.');
 } catch (error) {
-  console.error("Unable to connect to the database:", error);
+  console.error('Unable to connect to the database:', error);
 }
 
-console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
 
 app.listen(process.env.PORT, () => {
-  console.log("listen on port: ", 3001);
+  console.log('listen on port: ', process.env.PORT);
 });
