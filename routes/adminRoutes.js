d828dff6fc8919dbd239d4edb7858c496f2f394f
Khit Khom <khomkhit460@gmail.com>
2024-09-19 11:02:04 +0700

    feat-getme: create test for getadmin and route.

---
+++ routes/adminRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+const adminController = require('../controllers/adminController');
+// const errorController = require('../controllers/errorController')
+
+const router = express.Router();
+
+router.use(authController.protect);
+
+router.get(
+  '/getAdminInfo',
+  userController.getMe,
+  adminController.getAdditionalInfo,
+);
+
+module.exports = router;
