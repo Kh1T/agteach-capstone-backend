46751496d62ba0c0a35f7dacb59c262a8f668e17
seyla-song <songseyla99@gmail.com>
2024-09-23 10:14:01 +0700

    feat-get-api: create Product Model

--- models/productModel.js
+++ models/productModel.js
@@ -1,28 +1,53 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = require("../config/db");
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
 
-const ProductCategory = sequelize.define("product_category", {
-  categoryId: {
+const Product = sequelize.define('Product', {
+  product_id: {
     type: DataTypes.INTEGER,
-    autoIncrement: true,
     primaryKey: true,
+    autoIncrement: true,
+  },
+  instructor_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'instructors', // Name of the referenced table
+      key: 'instructor_id',
+    },
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product_categories', // Name of the referenced table
+      key: 'category_id',
+    },
   },
   name: {
     type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
     allowNull: false,
   },
   description: {
     type: DataTypes.TEXT,
     allowNull: false,
   },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+  image_url: {
+    type: DataTypes.TEXT,
   },
-  updatedAt: {
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  created_at: {
     type: DataTypes.DATE,
-    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    defaultValue: DataTypes.NOW,
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
   },
 });
 
-module.exports = ProductCategory;
+module.exports = Product;
