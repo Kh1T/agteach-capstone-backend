bee8e15408f88b889b666768b8c1291334140d91
AnB School 0031 <anbschool0031@AnBs-Mac-mini-100.local>
2024-09-23 18:01:10 +0700

    feat-get-api: my first controller

---
+++ controllers/productController.js
@@ -0,0 +1,16 @@
+const catchAsync = require('../utils/catchAsync');
+const Product = require('../models/productModel');
+const { get, all } = require('../routes/userRoutes');
+
+const allProduct = allProduct.get('name', 'price', 'instructor', 'image');
+
+exports.getAll = async(req, res, next) => {
+    
+    const allProduct = await Product.findAll()
+
+    res.status(200).json({
+        status: 'success',
+        results: allProduct.length,
+        allProduct
+    })
+}
