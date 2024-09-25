2fb889780e49af6356e87a1374fa260f982ad9ad
seyla-song <songseyla99@gmail.com>
2024-09-25 15:19:29 +0700

    feat-get: refactor code

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,5 +2,6 @@ const Product = require('../models/productModel');
 const handleFactory = require('./handlerFactory');
 
 exports.getAll = handleFactory.getAll(Product);
+exports.getOne = handleFactory.getOne(Product);
 exports.deleteOne = handleFactory.deleteOne(Product);
 exports.searchData = handleFactory.SearchData(Product);
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,12 +4,11 @@ const productController = require('../controllers/productController');
 
 const router = express.Router();
 
-router.use(authController.protect);
+// router.use(authController.protect);
 
 router.get('/getAllProduct', productController.getAll);
-
+router.get('/getOneProduct/:id', productController.getOne);
 router.delete('/deleteOneProduct/:id', productController.deleteOne);
-
 router.get('/searchData', productController.searchData);
 
 module.exports = router;
