190f75a485967404e07051bd0fadd19d77570c30
seyla-song <songseyla99@gmail.com>
2024-09-25 16:10:03 +0700

    testing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,7 +17,7 @@ router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:resetToken', authController.resetPassword);
 
 //  Protected Routes (Requires Authentication)
-// router.use(authController.protect);
+router.use(authController.protect);
 router.patch('/updatePassword', authController.updatePassword);
 router.post('/signup/additionalInfo', customerController.addAdditionalInfo);
 
