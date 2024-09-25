3dd487ebe9e057c3362f763ea299f63e5619303f
Khit Khom <khomkhit460@gmail.com>
2024-09-25 13:35:37 +0700

    feat-upload-product: add multer that can upload multiple images.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const productController = require('../controllers/productController');
+const authController = require('../controllers/authController');
+const { uploadProductImages } = require('../utils/multerConfig');
 
 const router = express.Router();
 
@@ -12,6 +14,9 @@ router.delete('/deleteOneProduct/:id', productController.deleteOne);
 router.get('/searchData', productController.searchData);
 router.get('/sortData', productController.sortData);
 
+router.use(authController.protect);
+
+router.post('/createProduct', uploadProductImages ,productController.createProduct);
 
 
 module.exports = router;
