d6c4b822110e7e88e8616087890d7b7d817c4851
seyla-song <songseyla99@gmail.com>
2024-09-26 16:21:36 +0700

    feat-get: uncommend serach controller

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -5,8 +5,8 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.get('/getAllCourse', courseController.getAll);
-// router.get('/getOneCourse/:id', courseController.getOne);
-// router.get('/searchData', courseController.searchData);
+router.get('/getOneCourse/:id', courseController.getOne);
+router.get('/searchData', courseController.searchData);
 // router.get('/sortData', courseController.sortData);
 
 module.exports = router;
