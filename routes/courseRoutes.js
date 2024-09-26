093fe845fb22b747716cd197c3d95d1b1f72e737
SokhaNeadh Rith <sokhaneadh@gmail.com>
2024-09-26 14:23:52 +0700

    feat-get-course: pull from the develop

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,4 +4,9 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.get('/getAllCourse', courseController.getAll);
+router.get('/getOneCourse/:id', courseController.getOne);
+router.get('/searchData', courseController.searchData);
+router.get('/sortData', courseController.sortData);
+
 module.exports = router;
