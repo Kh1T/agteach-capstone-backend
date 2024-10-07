e29e20f3982f5b169e301cc94b0cd9d1a9422238
seyla-song <songseyla99@gmail.com>
2024-10-07 14:50:40 +0700

    protect product detail

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,10 +8,10 @@ const router = express.Router();
 
 router.get('/searchData', productController.searchData);
 router.get('/getAllProduct', productController.getAll);
+router.get('/getProductDetail/:id', productController.getProductDetail);
 
 router.use(authController.protect, authController.restrictTo('instructor'));
 
-router.get('/getProductDetail/:id', productController.getProductDetail);
 router.delete('/deleteOneProduct/:id', productController.deleteOne);
 router.post(
   '/createProduct',
