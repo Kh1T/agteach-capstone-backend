865252877ac1a2e8bc38bae057ee597df99f0659
sela-bet <betsela555@gmail.com>
2024-10-07 15:28:46 +0700

    ref: wrap the handler function wiht catchAsync

--- controllers/enrollmentController.js
+++ controllers/enrollmentController.js
@@ -1,5 +1,7 @@
-exports.getCheckoutSession = (req, res, next) => {
+const catchAsync = require('../utils/catchAsync');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
   });
-};
+});
