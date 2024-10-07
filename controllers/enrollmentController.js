8c107c47f8861f46858e039817e7b522f3eb09d4
sela-bet <betsela555@gmail.com>
2024-10-07 15:30:24 +0700

    ref: import stripe to controller

--- controllers/enrollmentController.js
+++ controllers/enrollmentController.js
@@ -1,3 +1,4 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const catchAsync = require('../utils/catchAsync');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
