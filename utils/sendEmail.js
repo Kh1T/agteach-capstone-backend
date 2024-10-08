dcde1f31a510f84966b84d744ada26c6e78b8062
Khit Khom <khomkhit460@gmail.com>
2024-10-08 18:28:01 +0700

    feat-sendemail-api: change the template id name.

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,16 +1,14 @@
-const sgMail = require("@sendgrid/mail");
+const sgMail = require('@sendgrid/mail');
 
-const sendEmail = ({ email, emailVerifyCode, username, templateId }, options) => {
+const sendEmail = ({ email, emailVerifyCode, username }, options) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-  console.log(emailVerifyCode);
   // Generate verification code
   const code = options.code ? options.code : emailVerifyCode;
   const msg = {
     to: email,
     from: process.env.EMAIL_FROM,
     subject: options.subject,
-    templateId: templateId,
+    templateId: options.templateId,
     dynamicTemplateData: {
       username: username,
       code,
@@ -18,15 +16,13 @@ const sendEmail = ({ email, emailVerifyCode, username, templateId }, options) =>
     },
   };
 
-  console.log(msg.dynamicTemplateData.code);
-
   sgMail
     .send(msg)
     .then(() => {
-      console.log("Email sent successfully");
+      console.log('Email sent successfully');
     })
     .catch((error) => {
-      console.error("Error sending email:", error);
+      console.error('Error sending email:', error);
     });
 };
 
