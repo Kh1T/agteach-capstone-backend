00f0f0f7aa303db7859a32b92a8030be903cbadf
sela-bet <betsela555@gmail.com>
2024-10-08 14:54:44 +0700

    ref: add protect middleware to enrollmentRoute

--- routes/enrollmentRoutes.js
+++ routes/enrollmentRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const enrollmentController = require('../controllers/enrollmentController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/checkout-session', enrollmentController.getCheckoutSession);
+router.post(
+  '/checkout-session',
+  authController.protect,
+  enrollmentController.getCheckoutSession,
+);
 
 module.exports = router;
