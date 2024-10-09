f773244c9b954d7423387342310c019b69ce00a1
Khit Khom <khomkhit460@gmail.com>
2024-10-09 18:46:10 +0700

    feat-sendemail-api: debug on userAccount for send email.

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -87,7 +87,10 @@ exports.additionalInfo = (Model) =>
       data.email = req.user.email;
       const userData = await Model.create(data);
 
-      await sendEmail(this, {
+      const userAccount = await Model.findOne({
+        where: { userUid: req.user.userUid },
+      });
+      await sendEmail(userAccount, {
         templateId: process.env.SIGNUP_EMAIL_TEMPLATE_ID,
         subject: 'Your account has been created',
         text: `Your verification code is ${req.user.verificationCode}. Please enter this code on the verification page to complete your registration.`,
