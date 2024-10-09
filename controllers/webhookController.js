351cd1882e4442612664f17ad14457aee5c6b8e7
sela-bet <betsela555@gmail.com>
2024-10-09 15:41:46 +0700

    chore: add transaction record to enroll and course sale history

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -1,8 +1,36 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-const createCourseSaleHistory = () => {};
+const CourseSaleHistory = require('../models/courseSaleHistoryModel');
+const Enroll = require('../models/enrollModel');
 
-const createEnrollment = () => {};
+const createCourseSaleHistory = async (
+  courseId,
+  instructorId,
+  customerId,
+  price,
+) => {
+  try {
+    await CourseSaleHistory.create({
+      courseId,
+      instructorId,
+      customerId,
+      price,
+    });
+  } catch (err) {
+    console.log(`Somethin went wrong: ${err}`);
+  }
+};
+
+const createEnrollment = async (courseId, customerId) => {
+  try {
+    await Enroll.create({
+      courseId,
+      customerId,
+    });
+  } catch (err) {
+    console.log(`Somethin went wrong: ${err}`);
+  }
+};
 
 exports.webhookEnrollmentCheckout = (req, res, next) => {
   const sig = req.headers['stripe-signature'];
@@ -25,9 +53,15 @@ exports.webhookEnrollmentCheckout = (req, res, next) => {
     const session = event.data.object;
     const { courseId, instructorId, customerId } = session.metadata;
 
-    console.log(
-      `Payment completed for session: ${session.id} ${courseId} ${instructorId} ${customerId} ${session.amount_total / 100}`,
+    createCourseSaleHistory(
+      courseId,
+      instructorId,
+      customerId,
+      session.amount_total / 100,
     );
+    createEnrollment(courseId, customerId);
+
+    console.log(`Payment completed for session: ${session.id}`);
   }
   res.status(200).json({ received: true });
 };
