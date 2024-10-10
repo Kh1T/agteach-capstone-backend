9f098dc1fd87af371a66e7fca4b204f1b39a5f2f
sela-bet <betsela555@gmail.com>
2024-10-10 17:39:40 +0700

    chore: add stripe checkout session handler for purchased

--- controllers/purchasedController.js
+++ controllers/purchasedController.js
@@ -1,8 +1,57 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Customer = require('../models/customerModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getCheckoutSession = catchAsync(async (res, req, next) => {
-  res.json({
-    statu: 200,
-    message: 'This route is working',
+const REDIRECT_DOMAIN = 'https://agteach.site';
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  const { cartItems } = req.body;
+  const { email, userUid } = req.user;
+
+  const customer = await Customer.findOne({
+    where: { userUid: userUid },
+    attribute: ['customerId'],
+  });
+
+  if (!customer) {
+    return next(new AppError('Customer not found', 404));
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: cartItems.map((item) => ({
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: item.name,
+          images: [item.imageUrl],
+          metadata: {
+            product_id: item.productId, // Pass product ID here
+          },
+        },
+        unit_amount: item.price * 100, // amount in cents
+      },
+      quantity: item.quantity,
+    })),
+    customer_email: email,
+    client_reference_id: userUid,
+    mode: 'payment',
+    metadata: {
+      type: 'product', // Mark the session as a product purchase
+      customerId: customer.customerId,
+    },
+    success_url: `${REDIRECT_DOMAIN}/success-payment?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${REDIRECT_DOMAIN}/fail-payment`,
+  });
+
+  res.status(200).json({
+    id: session.id,
+    message: 'Hello World',
+    data: {
+      cartItems,
+      email,
+      customerId: customer.customerId,
+    },
   });
 });
--- routes/purchasedRoutes.js
+++ routes/purchasedRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const purchasedController = require('../controllers/purchasedController');
-// const authController = require('../controllers/authController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/productCheckoutSession', purchasedController.getCheckoutSession);
+router.post(
+  '/productCheckoutSession',
+  authController.protect,
+  purchasedController.getCheckoutSession,
+);
 
 module.exports = router;
