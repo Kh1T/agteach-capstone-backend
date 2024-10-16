78706e0ab64b467996c6e1cf568240a6b92f1e72
sela-bet <betsela555@gmail.com>
2024-10-16 12:57:41 +0700

    chore: checkout if all products exist

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,5 +1,6 @@
 const Product = require('../models/productModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getCartItems = catchAsync(async (req, res, next) => {
   const cartItems = req.body;
@@ -14,6 +15,10 @@ exports.getCartItems = catchAsync(async (req, res, next) => {
     },
   });
   
+  // Check if all products exist
+  if(products.length !== productIds.length) {
+    return next(new AppError('No product found with that ID', 404));
+  }
 
   res.status(200).json({ status: 'success', productIds, products });
 });
