eed6e85494366aa38b95e76a43ca09166db87231
sela-bet <betsela555@gmail.com>
2024-10-16 15:46:16 +0700

    chore: change route from GET to POST request

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,6 +3,6 @@ const cartController = require('../controllers/cartController');
 
 const router = express.Router();
 
-router.get('/getCartItems', cartController.getCartItems);
+router.post('/getCartItems', cartController.getCartItems);
 
 module.exports = router;
