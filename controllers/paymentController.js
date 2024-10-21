470479c60466bc724ea40b95f692bbebda450d65
sela-bet <betsela555@gmail.com>
2024-10-21 16:39:02 +0700

    chore: using predefined AppError

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,13 +1,12 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getPaymentSession = catchAsync(async (req, res, next) => {
   const { sessionId } = req.params;
 
   if (!sessionId) {
-    return res
-      .status(400)
-      .json({ status: 'fail', message: 'Session ID is required.' });
+    return next(new AppError('Session Id not found', 404));
   }
 
   const session = await stripe.checkout.sessions.retrieve(sessionId);
