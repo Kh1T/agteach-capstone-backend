da81e8e83f95ba61b7d462ea9a986bb469b9f186
sela-bet <betsela555@gmail.com>
2024-10-21 20:31:34 +0700

    chore: remove console log

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,18 +5,15 @@ const catchAsync = require('../utils/catchAsync');
 exports.getPaymentSession = catchAsync(async (req, res, next) => {
   const { sessionId } = req.params;
 
-  console.log(sessionId);
-
   if (!sessionId) {
     return next(new AppError('Session Id not found', 404));
   }
 
   const session = await stripe.checkout.sessions.retrieve(sessionId);
-  console.log('Seesion', session)
+
   const paymentIntent = await stripe.paymentIntents.retrieve(
     session.payment_intent,
   );
-  console.log('Payment Intent', paymentIntent)
 
   res.status(200).json({
     status: 'success',
