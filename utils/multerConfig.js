67616ce43b6151b92046e40ac2bd08391a644079
Pav Munyphalla <lalaphalla@gmail.com>
2024-11-26 11:44:44 +0700

    docs: update doc on multerConfig

--- utils/multerConfig.js
+++ utils/multerConfig.js
@@ -2,6 +2,14 @@ const multer = require('multer');
 const AppError = require('./appError');
 
 const multerStorage = multer.memoryStorage();
+/**
+ * Filters incoming files to allow only images.
+ *
+ * @param {object} req - The Express request object.
+ * @param {object} file - The file object containing information about the uploaded file.
+ * @param {function} cb - A callback function to indicate success or failure.
+ * @returns {void} Calls the callback with `null` and `true` if the file is an image, otherwise calls with an error.
+ */
 const imageFilter = (req, file, cb) => {
   if (!req.file && file.mimetype.startsWith('image')) {
     cb(null, true);
@@ -10,8 +18,16 @@ const imageFilter = (req, file, cb) => {
   }
 };
 
-// using req.files.productCover[0].buffer to get the buffer
-// This one was send to Resize Upload
+
+
+/**
+ * Middleware for uploading product images with multer.
+ *
+ * Configures multer to use memory storage, filters files to allow only images,
+ * and sets up fields for a single product cover image and up to four additional product images.
+ *
+ * @const {Multer} uploadProductImages
+ */
 const uploadProductImages = multer({
   storage: multerStorage,
   fileFilter: imageFilter,
@@ -20,12 +36,28 @@ const uploadProductImages = multer({
   { name: 'productImages', maxCount: 4 },
 ]);
 
-// This one was send to Resize Upload
+
+/**
+ * Middleware for uploading a user profile image with multer.
+ *
+ * Configures multer to use memory storage, filters files to allow only images,
+ * and sets a single file field named `photo`.
+ *
+ * @const {Multer} uploadProfileImage
+ */
 const uploadProfileImage = multer({
   storage: multerStorage,
   fileFilter: imageFilter,
-});
+}).single('photo');
 
+/**
+ * Filters incoming files to allow only videos or images.
+ *
+ * @param {object} req - The Express request object.
+ * @param {object} file - The file object containing information about the uploaded file.
+ * @param {function} cb - A callback function to indicate success or failure.
+ * @returns {void} Calls the callback with `null` and `true` if the file is a video or image, otherwise calls with an error.
+ */
 const videoFilter = (req, file, cb) => {
   if (
     (!req.file && file.mimetype.startsWith('video')) ||
@@ -37,6 +69,14 @@ const videoFilter = (req, file, cb) => {
   }
 };
 
+/**
+ * Middleware for uploading course videos with multer.
+ * 
+ * Configures multer to use memory storage, filters files to allow only videos,
+ * and sets a file size limit of 200MB.
+ *
+ * @const {Multer} uploadCourseVideosMulter
+ */
 const uploadCourseVideosMulter = multer({
   storage: multerStorage,
   fileFilter: videoFilter,
